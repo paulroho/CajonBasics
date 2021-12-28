1588e436fee7207760cdf65665eafab643ca774f
Paul Rohorzka <paul.rohorzka@squer.at>
2021-12-28 14:35:31 +0100

    Real data from the book (up to p. 49)

--- src/assets/data/cajon-basics.structure.js
+++ src/assets/data/cajon-basics.structure.js
@@ -2,85 +2,114 @@ module.exports = {
   "title": "CajonBasics",
   "pages": [
     {
-      "number": 1,
+      "number": 12,
+      "title": "Der cubanische Basston",
       "samples": [
         {
           "number": 1
         },
-        {
-          "number": 2
-        }
       ]
     },
     {
-      "number": 2,
+      "number": 13,
+      "title": "Der cubanische Randton (auch Open Tone genannt)",
+      "samples": [
+        {
+          "number": 2
+        },
+      ]
+    },
+    {
+      "number": 14,
+      "title": "Der peruanische Basston",
       "samples": [
         {
           "number": 3
         },
-        {
-          "number": 4
-        }
       ]
     },
     {
-      "number": 3,
+      "number": 15,
+      "title": "Der peruanische Randton (auch \"Tip\" genannt)",
+      "samples": [
+        {
+          "number": 4
+        },
+      ]
+    },
+    {
+      "number": 16,
+      "title": "Der Slap",
       "samples": [
         {
           "number": 5
         },
+      ]
+    },
+    {
+      "number": 17,
+      "title": "Kombinationen am Rand des Cajon (Tip, Open Tone, Tip und Slap)",
+      "samples": [
         {
           "number": 6
+        },
+      ]
+    },
+    {
+      "number": 18,
+      "samples": [
+        {
+          "number": 7,
+          "title": "Der Finger Roll",
+        },
+        {
+          "number": 8,
+          "title": "Die Krallhand",
         }
       ]
     },
     {
-      "number": 4,
+      "number": 19,
       "samples": [
         {
-          "number": 7
+          "number": 9,
+          "title": "Spiel mit den Fingerknöcheln",
         },
         {
-          "number": 8
-        }
+          "number": 10,
+          "title": "Spiel mit Fäusten",
+        },
+        {
+          "number": 11,
+          "title": "Beispiel \"Heavy Metal\"",
+        },
       ]
     },
     {
-      "number": 5,
+      "number": 20,
+      "title": "Glissando",
       "samples": [
-        {
-          "number": 9
-        },
-        {
-          "number": 10
-        }
-      ]
-    },
-    {
-      "number": 6,
-      "samples": [
-        {
-          "number": 11
-        },
         {
           "number": 12
-        }
+        },
       ]
     },
     {
-      "number": 7,
+      "number": 21,
+      "title": "Spiel mit Besen (Brushes)",
       "samples": [
         {
           "number": 13
         },
-        {
-          "number": 14
-        }
       ]
     },
     {
-      "number": 8,
+      "number": 22,
+      "title": "Spiel mit der Ferse - \"Hackentrick\"",
       "samples": [
+        {
+          "number": 14
+        },
         {
           "number": 15
         },
@@ -90,7 +119,8 @@ module.exports = {
       ]
     },
     {
-      "number": 9,
+      "number": 12,
+      "title": "Spiel mit Ruten (Rods)",
       "samples": [
         {
           "number": 17
@@ -101,146 +131,562 @@ module.exports = {
       ]
     },
     {
-      "number": 10,
+      "number": 24,
       "samples": [
         {
           "number": 19
         },
         {
           "number": 20
-        }
-      ]
-    },
-    {
-      "number": 11,
-      "samples": [
+        },
         {
           "number": 21
         },
-        {
-          "number": 22
-        }
       ]
     },
     {
-      "number": 12,
+      "number": 27,
+      "title": "Drum-Set-Grooves",
       "samples": [
+        {
+          "number": 34,
+          "playalong": true,
+        },
+        {
+          "number": 35,
+          "playalong": true,
+        },
+        {
+          "number": 22
+        },
         {
           "number": 23
         },
-        {
-          "number": 24
-        }
       ]
     },
     {
-      "number": 13,
+      "number": 28,
       "samples": [
+        {
+          "number": 24
+        },
         {
           "number": 25
         },
         {
           "number": 26
-        }
-      ]
-    },
-    {
-      "number": 14,
-      "samples": [
+        },
         {
           "number": 27
         },
         {
           "number": 28
-        }
+        },
       ]
     },
     {
-      "number": 15,
+      "number": 29,
       "samples": [
         {
           "number": 29
         },
         {
           "number": 30
-        }
-      ]
-    },
-    {
-      "number": 16,
-      "samples": [
+        },
         {
           "number": 31
         },
         {
           "number": 32
-        }
+        },
+        {
+          "number": 33
+        },
       ]
     },
     {
       "number": 17,
+      "title": "Drum-Set-Grooves mit durchlaufender rechter Hand",
       "samples": [
         {
-          "number": 33
+          "number": 34,
+          "playalong": true,
         },
-        {
-          "number": 34
-        }
       ]
     },
     {
-      "number": 18,
+      "number": 31,
       "samples": [
         {
-          "number": 35
+          "number": 36,
+          "title": "1. Rods",
+          "start": "0:00",
+          "end": "0:29"
         },
         {
-          "number": 36
-        }
+          "number": 36,
+          "title": "1. Besen",
+          "start": "0:29",
+          "end": "0:57"
+        },
+        {
+          "number": 36,
+          "title": "1. Tips",
+          "start": "0:57",
+          "end": "1:24"
+        },
+        {
+          "number": 36,
+          "title": "2.",
+          "start": "1:24",
+          "end": "1:52"
+        },
+        {
+          "number": 36,
+          "title": "3.",
+          "start": "1:52",
+          "end": "2:20"
+        },
+        {
+          "number": 36,
+          "title": "4.",
+          "start": "2:20",
+          "end": "2:47"
+        },
       ]
     },
     {
-      "number": 19,
+      "number": 32,
       "samples": [
         {
-          "number": 37
+          "number": 36,
+          "title": "5.",
+          "start": "2:47",
+          "end": "3:15"
         },
         {
-          "number": 38
-        }
+          "number": 36,
+          "title": "6.",
+          "start": "3:15",
+          "end": "3:42"
+        },
+        {
+          "number": 36,
+          "title": "7.",
+          "start": "3:42",
+          "end": "4:09"
+        },
+        {
+          "number": 36,
+          "title": "8.",
+          "start": "4:09",
+          "end": "4:36"
+        },
       ]
     },
     {
-      "number": 20,
+      "number": 33,
       "samples": [
         {
-          "number": 39
+          "number": 36,
+          "title": "9.",
+          "start": "4:36",
+          "end": "5:03"
         },
         {
-          "number": 40
-        }
+          "number": 36,
+          "title": "10.",
+          "start": "5:03",
+          "end": "5:31"
+        },
+        {
+          "number": 36,
+          "title": "11.",
+          "start": "5:31",
+          "end": "5:58"
+        },
+        {
+          "number": 36,
+          "title": "12.",
+          "start": "5:58",
+        },
       ]
     },
     {
-      "number": 21,
+      "number": 34,
+      "title": "Triolen in der rechten Hand",
       "samples": [
         {
-          "number": 41
+          "number": 68,
+          "playalong": true,
         },
         {
-          "number": 42
-        }
+          "number": 37,
+          "title": "1.",
+          "end": "0:28"
+        },
+        {
+          "number": 37,
+          "title": "2.",
+          "start": "0:28",
+          "end": "0:57"
+        },
+        {
+          "number": 37,
+          "title": "3.",
+          "start": "0:57",
+          "end": "1:25"
+        },
+        {
+          "number": 37,
+          "title": "4.",
+          "start": "1:25",
+          "end": "1:52"
+        },
       ]
     },
     {
-      "number": 22,
+      "number": 35,
       "samples": [
         {
-          "number": 43
+          "number": 37,
+          "title": "5.",
+          "start": "1:52",
+          "end": "2:20"
         },
         {
-          "number": 44
-        }
+          "number": 37,
+          "title": "6.",
+          "start": "2:20",
+          "end": "2:47"
+        },
+        {
+          "number": 37,
+          "title": "7.",
+          "start": "2:47",
+          "end": "3:13"
+        },
+        {
+          "number": 37,
+          "title": "8.",
+          "start": "3:13",
+          "end": "3:41"
+        },
+      ]
+    },
+    {
+      "number": 36,
+      "samples": [
+        {
+          "number": 37,
+          "title": "9.",
+          "start": "3:41",
+          "end": "4:08"
+        },
+        {
+          "number": 37,
+          "title": "10.",
+          "start": "4:08",
+          "end": "4:36"
+        },
+        {
+          "number": 37,
+          "title": "11.",
+          "start": "4:36",
+          "end": "5:04"
+        },
+        {
+          "number": 37,
+          "title": "12.",
+          "start": "5:04",
+        },
+      ]
+    },
+    {
+      "number": 37,
+      "title": "Sechzehntel in der rechten Hand",
+      "samples": [
+        {
+          "number": 57,
+          "playalong": true,
+        },
+        {
+          "number": 38,
+          "title": "1.",
+          "end": "0:22"
+        },
+        {
+          "number": 38,
+          "title": "2.",
+          "start": "0:22",
+          "end": "0:47"
+        },
+        {
+          "number": 38,
+          "title": "3.",
+          "start": "0:47",
+          "end": "1:15"
+        },
+      ]
+    },
+    {
+      "number": 38,
+      "samples": [
+        {
+          "number": 38,
+          "title": "5.",
+          "start": "1:43",
+          "end": "2:10"
+        },
+        {
+          "number": 38,
+          "title": "6.",
+          "start": "2:10",
+          "end": "2:37"
+        },
+        {
+          "number": 38,
+          "title": "7.",
+          "start": "2:37",
+          "end": "3:04"
+        },
+        {
+          "number": 38,
+          "title": "8.",
+          "start": "3:04",
+          "end": "3:31"
+        },
+      ]
+    },
+    {
+      "number": 39,
+      "samples": [
+        {
+          "number": 38,
+          "title": "9.",
+          "start": "3:31",
+          "end": "3:58"
+        },
+        {
+          "number": 38,
+          "title": "10.",
+          "start": "3:58",
+          "end": "4:26"
+        },
+        {
+          "number": 38,
+          "title": "11.",
+          "start": "4:26",
+          "end": "4:54"
+        },
+        {
+          "number": 38,
+          "title": "12.",
+          "start": "4:54",
+        },
+      ]
+    },
+    {
+      "number": 40,
+      "title": "Handsätze und Kombinationen",
+      "samples": [
+        {
+          "number": 39,
+          "title": "1.",
+        },
+        {
+          "number": 40,
+          "title": "2.",
+        },
+        {
+          "number": 41,
+          "title": "3.",
+        },
+      ]
+    },
+    {
+      "number": 41,
+      "title": "Kombinationen aller dreri Übungen",
+      "samples": [
+        {
+          "number": 42,
+          "title": "4.",
+        },
+        {
+          "number": 43,
+          "title": "5.",
+        },
+        {
+          "number": 44,
+          "title": "6.",
+        },
+        {
+          "number": 45,
+          "title": "7.",
+        },
+      ]
+    },
+    {
+      "number": 42,
+      "title": "Abwechselnde Schlagfolge (Hand-to-Hand)",
+      "samples": [
+        {
+          "number": 46,
+          "title": "Basisfigur",
+        },
+        {
+          "number": 47,
+          "title": "1.",
+        },
+        {
+          "number": 48,
+          "title": "2.",
+        },
+        {
+          "number": 49,
+          "title": "3.",
+        },
+      ]
+    },
+    {
+      "number": 43,
+      "samples": [
+        {
+          "number": 50,
+          "title": "4.",
+        },
+        {
+          "number": 51,
+          "title": "5.",
+        },
+        {
+          "number": 52,
+          "title": "6.",
+        },
+        {
+          "number": 53,
+          "title": "7.",
+        },
+        {
+          "number": 54,
+          "title": "8.",
+        },
+      ]
+    },
+    {
+      "number": 44,
+      "samples": [
+        {
+          "number": 55,
+          "title": "9.",
+        },
+        {
+          "number": 56,
+          "title": "10.",
+        },
+        {
+          "title": "11.",
+        },
+        {
+          "title": "12.",
+        },
+      ]
+    },
+    {
+      "number": 45,
+      "title": "Groove und Fill (Rhythmus und Variation)",
+      "samples": [
+        {
+          "number": 57,
+          "title": "Basisrhythmus (TODO: check - missing qr-code in book)",
+        },
+        {
+          "number": 58,
+          "title": "Variation 1",
+        },
+        {
+          "number": 59,
+          "title": "Variation 2",
+        },
+      ]
+    },
+    {
+      "number": 46,
+      "samples": [
+        {
+          "number": 60,
+          "title": "Variation 3",
+        },
+        {
+          "number": 61,
+          "title": "Variation 4",
+        },
+        {
+          "number": 62,
+          "title": "Variation 5",
+        },
+      ]
+    },
+    {
+      "number": 47,
+      "title": "Triolen",
+      "samples": [
+        {
+          "number": 63,
+          "title": "Basisfigur",
+        },
+        {
+          "number": 68,
+          "playalong": true,
+        },
+        {
+          "number": 64,
+          "title": "1.",
+        },
+        {
+          "number": 65,
+          "title": "2.",
+        },
+      ]
+    },
+    {
+      "number": 48,
+      "samples": [
+        {
+          "number": 66,
+          "title": "3.",
+        },
+        {
+          "number": 67,
+          "title": "4.",
+        },
+        {
+          "title": "5.",
+        },
+        {
+          "title": "6.",
+        },
+        {
+          "title": "7.",
+        },
+      ]
+    },
+    {
+      "number": 49,
+      "title": "Neue Handsätze (Stickings)",
+      "samples": [
+        {
+          "title": "8.",
+        },
+        {
+          "title": "9.",
+        },
+        {
+          "title": "10.",
+        },
+        {
+          "title": "11.",
+        },
       ]
     },
     {
